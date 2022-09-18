27aa8dcdaaca8e0fa3451ab813f47ece77ce81c8
Marios Tsekitsidis <tseki@iastate.edu>
2022-09-18 09:42:09 -0500

    feat: successfully sent UDP beacon

--- src/agent/SenderUDP.c
+++ src/agent/SenderUDP.c
@@ -17,7 +17,7 @@ struct BEACON {
 int main(int argc, char *argv[]) {
     int s;
     struct sockaddr_in udpServer;
-    char buf[16]; // (8 bytes for the header) 16 bytes for the payload.
+    int8_t buf[16]; // (8 bytes for the header) 16 bytes for the payload.
 
     // Convert IP address from string to in_addr_t:
     in_addr_t ipOfUDPServerAsInAddrT;
@@ -50,10 +50,30 @@ int main(int argc, char *argv[]) {
         beacon.IP[i] = 1;
     }
 
-    strcpy(buf, "Hello");
+    // Load buf:
+    // The ID:
+    buf[0] = beacon.ID & 0xFF;
+    buf[1] = (beacon.ID >> 8) & 0xFF;
+    buf[2] = (beacon.ID >> 16) & 0xFF;
+    buf[3] = (beacon.ID >> 24) & 0xFF;
+    // The StartUpTime:
+    buf[4] = beacon.StartUpTime & 0xFF;
+    buf[5] = (beacon.StartUpTime >> 8) & 0xFF;
+    buf[6] = (beacon.StartUpTime >> 16) & 0xFF;
+    buf[7] = (beacon.StartUpTime >> 24) & 0xFF;
+    // The IP:
+    buf[8]  = beacon.IP[0];
+    buf[9]  = beacon.IP[1];
+    buf[10] = beacon.IP[2];
+    buf[11] = beacon.IP[3];
+    // The CmdPort:
+    buf[12] = beacon.CmdPort & 0xFF;
+    buf[13] = (beacon.CmdPort >> 8) & 0xFF;
+    buf[14] = (beacon.CmdPort >> 16) & 0xFF;
+    buf[15] = (beacon.CmdPort >> 24) & 0xFF;
 
     // Send the message in buf to the server:
-    if (sendto(s, buf, (strlen(buf)+1), 0, (struct sockaddr *)&udpServer, sizeof(udpServer)) < 0) {
+    if (sendto(s, buf, sizeof(buf), 0, (struct sockaddr *)&udpServer, sizeof(udpServer)) < 0) {
         perror("sendto()");
         exit(2);
     }
